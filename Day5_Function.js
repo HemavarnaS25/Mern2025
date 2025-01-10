0dd865052f0f37512051602d2f51e6a4e699f3fd
Hemavarna S <hemavarnas25@gmail.com>
2025-01-10 22:51:06 +0530

    Add files via upload
    Day 5 task file is added

---
+++ Day5_Function.js
@@ -0,0 +1,24 @@
+//with parameter and with return
+function SECE(dept){
+    return "Section of the department is A";
+}
+console.log(SECE("cse"));
+
+//with parameter and without return
+function SECE2025(dept){
+    console.log("The dept is : ",dept);
+}
+SECE("cse");
+
+//without parameter and with return
+function Add(){
+    return "Addion";
+}
+console.log(Add());
+
+//without parameter and without return
+function sub(){
+    console.log("Subtraction");
+
+}
+sub();
