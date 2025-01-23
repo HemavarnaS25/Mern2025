5075c4b2744a0b4f1b79e7be2b39769d5522f5bd
HemavarnaS25 <hemavarnas25@gmail.com>
2025-01-23 17:29:48 +0530

    Restructured Codebase

---
+++ Javascript/Day4_loop.js
@@ -0,0 +1,47 @@
+// var b =7
+
+// for(let [i,j] of Object.values(b)) {
+//     console.log(i,j);
+// }
+// a=[10,20,30,40]
+// b=[50,60,70]
+// c=[...b,a]
+// console.log(c)
+// console.log(b,a);
+// //rest Operator
+// function restOpEg(a,b,...arg){
+//     console.log(a)
+//     console.log(b)
+//     console.log(arg)
+// }
+// restOpEg(10,20,30,40)
+
+// 8.Destructing Operators
+// var array=[10,20,30]
+// var [a,b,c,d]=array
+// console.log(a,b,c,d)
+
+//9.Hoisting Operators
+// 1.Variable Hoisting
+// console.log(a)
+// var a =10;
+// console.log(a)
+//  console.log(a)
+//  let a = 10
+//  console.log(a)
+//  const a =10
+//  console.log(b)
+//  const b =20
+//  const c=30
+//  console.log(c)
+
+//2.Function Hoisting'
+
+// console.log(fun())
+// function fun(){
+//     return 10
+// }
+
+
+
+
